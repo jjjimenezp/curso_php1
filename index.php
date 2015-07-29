23aee00826f2146e9e156854a3b93db2d9168556
jjjimenezp <jjjimenezp@gmail.com>
2015-07-28 19:41:15 -0500

    conexiona base de datos
    Conexion a la base de datos

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
 <?php
-echo "hola mundo";
-?>
+echo "Conexión a la base de datos";
+$URL = "sql2.freemysqlhosting.net";
+$database = "sql285295";
+$usuario = "sql285295";
+$password = vS3%wL8%;
+$port = 3306;
+// CREAR CONECCION
+$conexion = mysql_connect(            
+	                  $url,
+	                  $usuario,
+	                  $password,
+	                  $database,
+	                  $port);
+if (!$conexion){
+	die (Finalizado la conexion");
+}
+echo "Conexion exitosa";
+</php>

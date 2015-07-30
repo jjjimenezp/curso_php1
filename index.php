58e7f6a976e608efc3e7c95eb60ca5e6bd006809
ariel vernaza <ariel.vernaza@dsalab.com>
2015-07-29 21:01:56 -0400

    adding change

--- index.php
+++ index.php
@@ -23,12 +23,12 @@ while ($row= mysqli_fetch_assoc($result)){
 	echo $row ['id']." ".$row['nombre']. " ". 
 	$row['descripcion'];
 }
-mysqli_close(conexion);
-$response = Unirest/Request::get
-("https://bestapi-waze-unoffical-free-v1
-	p.mashape.com/addressList?address=universidad+panama",
-	array (
-		"x-Mashape-Key" => "ZA8k3CJvxdmshT0XPS3S6WV6vnfwp1hj5F9jsnNujBw5cKjh2Y,
-		"Accept"=>"application/json"));
-echo $response;
+
+$response = Unirest\Request::get("https://bestapi-waze-unoffical-free-v1.p.mashape.com/addressList?address=Universidad+de+Panama",
+  array(
+    "X-Mashape-Key" => "ZA8k3CJvxdmshT0XPS3S6WV6vnfwp1hj5F9jsnNujBw5cKjh2Y",
+    "Accept" => "application/json"
+  )
+);
+echo  $response->raw_body;
 ?>

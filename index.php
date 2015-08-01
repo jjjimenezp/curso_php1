f7ecb31d2cd56e2bb6e1df2809d164b4cfa29d7e
ariel vernaza <ariel.vernaza@dsalab.com>
2015-07-31 21:16:04 -0400

    final

--- index.php
+++ index.php
@@ -1,49 +1,42 @@
-<?php
-require __DIR__. '/vendor/autoload.php';
-echo "Conexión a la base de datos";
-$URL = "sql2.freemysqlhosting.net";
-$database = "sql285295";
-$usuario = "sql285295";
-$password = "vS3%wL8%";
-$port = "3306";
-// CREAR CONECCION
-$conexion = mysqli_connect(            
-	                  $URL,
-	                  $usuario,
-	                  $password,
-	                  $database,
-	                  $port);
-if (!$conexion){
-	die ("Finalizado la conexion");
-}
-echo "Conexion exitosa";
-$query ="select * from demo";
-$result = mysqli_query($conexion,$query);
-while ($row= mysqli_fetch_assoc($result)){
-	echo $row ['id']." ".$row['nombre']. " ". 
-	$row['descripcion'];
-}
+<html>
+<form>
+  <head>
+    <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 
-$response = Unirest\Request::get("https://bestapi-waze-unoffical-free-v1.p.mashape.com/addressList?address=Universidad+de+Panama",
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+    </head>
+<body>
+<form name="input" action="index.php" method="POST">
+ <div class="input-group">
+   <span class="input-group-btn">
+    <input type="submit" type="button">IR!</button>
+    </span>
+    <input type="text" name="word" class="form-control" placeholder="search for...">
+   </div>
+</form>
+<?php
+require  __DIR__.'/vendor/autoload.php';
+
+$response = Unirest\Request::get("https://montanaflynn-dictionary.p.mashape.com/define?word=".$_GET["word"],
   array(
     "X-Mashape-Key" => "ZA8k3CJvxdmshT0XPS3S6WV6vnfwp1hj5F9jsnNujBw5cKjh2Y",
     "Accept" => "application/json"
   )
 );
-echo  $response->raw_body;
-// These code snippets use an open-source library. http://unirest.io/php
-$response = Unirest\Request::get("https://bestapi-waze-unoffical-free-v1.p.mashape.com/routes?end=156+5th+Avenue%2C+New+York%2C+NY+10010&start=6+East+57th+Street%2C+New+York%2C+NY+10022",
-  array(
-    "X-Mashape-Key" => "ZA8k3CJvxdmshT0XPS3S6WV6vnfwp1hj5F9jsnNujBw5cKjh2Y",
-    "Accept" => "application/json"
-  )
-);
-echo  $response->raw_body;
-$response = Unirest\Request::get("https://montanaflynn-dictionary.p.mashape.com/define?word=test",
-  array(
-    "X-Mashape-Key" => "ZA8k3CJvxdmshT0XPS3S6WV6vnfwp1hj5F9jsnNujBw5cKjh2Y",
-    "Accept" => "application/json"
-  )
-);
-echo  $response->raw_body;
+echo "<ul class=\"nav nav-pills\">";
+foreach($response->body->definitions as $definition)
+{
+ echo "<li role=\"presentation\"><a href=\"#\">".$definition->text."</a></li>";
+}
+ echo "</ul>";
 ?>
+ </div>
+</div>
+</body> 
+</html>
